441779233e619bc668ca8a3fa02d976590621567
Palash Bansal <PalashBansal96@gmail.com>
2016-11-27 17:08:44 +0530

    Removed old code

--- material.cpp
+++ material.cpp
@@ -19,13 +19,6 @@ float xorshf96() {          //3.6 times better performance than rand()
 	return 1.0f*z/0xffffffff;
 }
 
-Vector3D randomRotate(Vector3D vector, float angle){
-	angle = (rand()%360)*angle/180-angle; angle = angle*M_PI/180;
-	Vector3D randVector(1+(rand()%100), 1+(rand()%100), 1+(rand()%100));
-	Vector3D k = crossProduct(randVector, vector);
-	return Vector3D(vector*cos(angle) + crossProduct(k, vector)*sin(angle) + k*dotProduct(k, vector)*(1-cos(angle))); //Rodrigue's rotation formula
-}
-
 Color Material::shade(const Ray& incident, const bool isSolid) const
 {
 	if(!incident.didHit()) return world->getBackground();
@@ -107,69 +100,4 @@ Color Material::shade(const Ray& incident, const bool isSolid) const
 	}
 	return finalColor;
 
-	const std::vector<LightSource *> &lights = world->getLightSourceList();
-	bool isInside = cosTheta > 0;
-	if(isInside) cosTheta = dotProduct(incident.getDirection(), -incident.getNormal());
-
-	float lobe = (float) (kr * kr);
-	Color reflectedColor(0);
-	float wtSum = 0;
-
-	if(rand()%3)
-	{
-		reflectedColor = lobe*world->shade_ray(reflectedRay); wtSum = lobe;
-	}
-	for(int i=0;i<kr*4;i++) {
-		Vector3D randDirection = randomRotate(incident.getNormal(), 45);
-		lobe = (float) pow(std::max(dotProduct(randDirection, incident.getNormal()), 0.), n);
-		Ray _reflectedRay = Ray(incident.getPosition(), randDirection,
-		                        incident.getLevel() + 1);
-		reflectedColor = reflectedColor + lobe*world->shade_ray(_reflectedRay);
-		wtSum += lobe;
-	}
-	reflectedColor = reflectedColor / wtSum;
-	double reflectance = pow((eta-1)/(eta+1), 2);
-	double schlickApp = reflectance + (1 - reflectance) * pow(1 + cosTheta, 1);
-	if(isSolid && kt && eta && incident.getLevel() < MAX_LEVEL){ //Do refraction
-		double currentEta = isInside?1:eta;
-		Vector3D direction = incident.getRefractive_index() * (incident.getDirection() - incident.getNormal()*cosTheta) / currentEta;
-		direction = direction - incident.getNormal() * sqrt(1-(incident.getRefractive_index()*incident.getRefractive_index()*(1-cosTheta*cosTheta)/(currentEta*currentEta)));
-		direction.normalize();
-		Ray refractedRay = Ray(incident.getPosition(), direction, incident.getLevel()+1, (float) currentEta);
-		Color beerK(1);
-		if(isInside) {
-			world->firstIntersection(refractedRay);
-			double t = (refractedRay.getPosition() - incident.getPosition()).length()/5;
-			beerK.r = exp(-color.r*t);
-			beerK.g = exp(-color.g*t);
-			beerK.b = exp(-color.b*t);
-		}
-		finalColor = finalColor + ((kr * schlickApp) * reflectedColor + beerK * kt * world->shade_ray(refractedRay));
-	}else {
-		finalColor = finalColor + (kr + schlickApp) * reflectedColor;
-	}
-	finalColor.clamp();
-	finalColor = finalColor * color;
-	Color myColor = color + finalColor;
-	myColor.clamp();
-	for(int i=0; i<lights.size(); i++){
-		LightSource* light = lights[i];
-		Vector3D l = incident.getPosition() - light->getPosition();
-		Vector3D r = l - 2 * incident.getNormal() * dotProduct(l, incident.getNormal());
-
-		Ray shadowRay = Ray(incident.getPosition(), -l, incident.getLevel()+1);
-		world->firstIntersection(shadowRay);
-		if(!shadowRay.didHit() || shadowRay.intersected()->getLightSource() != light) continue;
-
-		double attenuation = 1.0f / (1.0f + katt * pow(l.length(), 2));
-		r.normalize();
-		l.normalize();
-
-		Color diffuse = myColor * light->getIntensity() * std::max( dotProduct(incident.getNormal(), -l), 0.0);
-		Color specular = light->getIntensity() * pow( std::max(dotProduct(r, -incident.getDirection()), 0.0), n);
-		finalColor = finalColor + attenuation * (kd * diffuse + ks * specular);
-
-		finalColor.clamp();
-	}
-	return finalColor;
 }
